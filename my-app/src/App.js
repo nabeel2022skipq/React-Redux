c3e64a7eedac7f2dbebc124df0ac39d82db77601
nabeelahmed-0 <nabeelahmedpk1@outlook.com>
2022-10-24 00:31:19 +0500

    Contact Manager App

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,8 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import React, { useState } from 'react';
-
-function AddPerson(){
+function AddPerson(props){
 
   const [name, setName]=useState("")
 
@@ -11,11 +10,17 @@ function AddPerson(){
     setName(e.target.value)
   }
 
+  function handleSubmit(e)
+  {
+    props.handleSubmit(name)
+    setName("")
+    e.preventDefault()
+  }
 
   return(
     <React.Fragment>
-      <form onSubmit={handleChange}>
-        <input type="text" placeholder="Enter New Contact" value={name}></input>
+      <form onSubmit={handleSubmit}>
+        <input placeholder="Enter New Contact" value={name} onChange={handleChange}></input>
         <button type='submit'>Add</button>
       </form>
     </React.Fragment>
@@ -34,13 +39,30 @@ function PersonList(props)
   )
 }
 
+function ContactManager(props)
+{
+
+  const [contacts, setContacts]=useState(props.data)
+
+  function addContact(name)
+  {
+    return setContacts([...contacts,name])
+  }
+  return(
+    <React.Fragment>
+      <AddPerson handleSubmit={addContact}></AddPerson>,
+      <PersonList data={contacts}></PersonList>
+    </React.Fragment>
+  )
+  
+}
+
 function App() {
   const data=["Nabeel","Hammad","Ammar","Raheel"]
   return (
     <div className="App">
       <h1>Contact Manager</h1>
-      <AddPerson></AddPerson>
-      <PersonList data={data}></PersonList>
+      <ContactManager data={data}></ContactManager>
     </div>
   );
 }
