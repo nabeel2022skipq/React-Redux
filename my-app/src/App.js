b987ba3790adc5613de3e6e7ee2a16b018441b78
nabeelahmed-0 <nabeelahmedpk1@outlook.com>
2022-10-24 00:15:57 +0500

    Add Person And Person List

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+
+function AddPerson(){
+
+  const [name, setName]=useState("")
+
+  function handleChange(e)
+  {
+    setName(e.target.value)
+  }
+
+
+  return(
+    <React.Fragment>
+      <form onSubmit={handleChange}>
+        <input type="text" placeholder="Enter New Contact" value={name}></input>
+        <button type='submit'>Add</button>
+      </form>
+    </React.Fragment>
+  )
+}
+
+function PersonList(props)
+{
+
+  const arr=props.data
+  const itemsList=arr.map((value, index) => <li key={index}>{value}</li>)
+  return(
+    <React.Fragment>
+      <ul>{itemsList}</ul>
+    </React.Fragment>
+  )
+}
 
 function App() {
+  const data=["Nabeel","Hammad","Ammar","Raheel"]
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Contact Manager</h1>
+      <AddPerson></AddPerson>
+      <PersonList data={data}></PersonList>
     </div>
   );
 }
