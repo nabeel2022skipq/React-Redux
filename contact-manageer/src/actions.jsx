40c0f5b637c3f00a47a8c573cb3d8258e4810be7
nabeelahmed-0 <nabeelahmedpk1@outlook.com>
2022-10-25 01:37:19 +0500

    Actions & Reducer

--- contact-manageer/src/actions.jsx
+++ contact-manageer/src/actions.jsx
@@ -1,3 +1,7 @@
+//Action function which will dispatch the action
+//to the reducer function which will change the app
+//state on the basis of the action type
+
 export function addContact(person) {
     return {
         type: "ADD_CONTACT",
